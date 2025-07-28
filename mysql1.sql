4a49ce9daa29c1bdb02c519f780a878aacaa1151
Asmashaik7 <asmashaik.nov11@gmail.com>
2025-07-28 14:51:31 +0530

    Add files via upload

---
+++ mysql1.sql
@@ -0,0 +1,8 @@
+-- create table emp(emp_id integer NOT NULL PRIMARY KEY,
+-- name TEXT NOT NULL,
+-- email NOT NULL); 
+
+-- INSERT INTO emp(emp_id,name,email)
+-- VALUES (7,'sk','sk@gmail.com')
+
+SELECT * FROM emp;
